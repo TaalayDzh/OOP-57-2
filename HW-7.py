da8793b46d3f17125e12dd1b5390e6ff99148707
TaalayDzh <taalay.dzhumashev@yandex.ru>
2025-08-29 10:59:45 +0600

    HW-7

--- HW-7.py
+++ HW-7.py
@@ -28,46 +28,46 @@ def add_user(name_1, age_1, hobby_1):
     connect.commit()
     print('Пользователь добавлен!!')
 
-add_user('Андрей', 15, "Летать")
-add_user('Ardager', 25, "Летать")
-add_user('Arzy', 26, "Летать")
-add_user('Oleg', 12, "Летать")
-add_user('Alex', 33, "Летать")
+# add_user('Андрей', 15, "Летать")
+# add_user('Ardager', 25, "Летать")
+# add_user('Arzy', 26, "Летать")
+# add_user('Oleg', 12, "Летать")
+# add_user('Alex', 33, "Летать")
 
 
 
 
-# def get_users():
-#     cursor.execute('SELECT name, age, hobby FROM users')
-#     users = cursor.fetchall()
-#
-#     for i in users:
-#         print(f"NAME: {i[0]}, AGE: {i[1]}, HOBBY: {i[2]}")
-#
-#
-#
-# def update_user(name, row_id):
-#     # cursor.execute(
-#     #     'UPDATE users SET name = ? WHERE rowid = ?',
-#     #     (name, row_id)
-#     # )
-#     cursor.execute(f'UPDATE users SET name = "{name}" WHERE rowid = "{row_id}"')
-#     connect.commit()
-#     print('Пользователь обнавлен')
-#
-# update_user("Arzy", 1)
+def get_users():
+    cursor.execute('SELECT name, age, hobby FROM users')
+    users = cursor.fetchall()
+
+    for i in users:
+        print(f"NAME: {i[0]}, AGE: {i[1]}, HOBBY: {i[2]}")
+
+
+
+def update_user(name, row_id):
+    # cursor.execute(
+    #     'UPDATE users SET name = ? WHERE rowid = ?',
+    #     (name, row_id)
+    # )
+    cursor.execute(f'UPDATE users SET name = "{name}" WHERE rowid = "{row_id}"')
+    connect.commit()
+    print('Пользователь обнавлен')
+
+update_user("Arzy", 1)
 
 
 def delete_user(row_id):
-    cursor.execute(
-        'DELETE FROM users WHERE rowid = ?',
-        (row_id,)
-                )
+    # cursor.execute(
+    #     'DELETE FROM users WHERE rowid = ?',
+    #     (row_id,)
+    # )
 
-cursor.execute(f'DELETE FROM users WHERE id = "{row_id}"')
-connect.commit()
-print('Пользоватлеь удален!')
+    cursor.execute(f'DELETE FROM users WHERE id = "{row_id}"')
+    connect.commit()
+    print('Пользоватлеь удален!')
 
-delete_user(1)
+delete_user(4)
 
 get_users()
